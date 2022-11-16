b58b246a7d844cd6d8720398e6285c8eb0158906
Rihards <reiniksrihards@gmail.com>
2022-11-16 16:46:40 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClashComponent } from './clash/clash.component';
+import { AramComponent } from './howlingabyss/aram/aram.component';
+import { MasteryComponent } from './mastery/mastery.component';
 import { SummonerComponent } from './summoner/summoner.component';
+import { SummonersriftComponent } from './summonersrift/summonersrift.component';
 
 const routes: Routes = [
   {path: "", pathMatch: "full", redirectTo: '/summoner'},
-  {path: "summoner", component: SummonerComponent}
+  {path: "summoner", component: SummonerComponent},
+  {path: "rift", component: SummonersriftComponent},
+  {path: "ARAM", component: AramComponent},
+  {path: "clash", component: ClashComponent},
+  {path: "mastery", component: MasteryComponent}
 ];
 
 @NgModule({
